f089cb15ed6709f65ea0f2386ded968c01684a42
Zack Wall <zacharyleviwall@gmail.com>
2020-04-28 16:39:29 -0400

    Initial Commit

---
+++ Tasks/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tasks
+//
+//  Created by Zack Wall on 4/28/20.
+//  Copyright © 2020 Zack Wall. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
